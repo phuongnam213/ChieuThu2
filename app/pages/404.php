3743f40bce0a4d25ddee5c0979f0e3c839b071df
Tuandeptrai <ADMIN@DESKTOP-PRQ4TMA>
2025-04-05 21:50:00 +0700

    Remove all files

--- app/pages/404.php
+++
@@ -1,8 +0,0 @@
-<?php require page('includes/header')?>
-	
-	<center>
-		<img src="<?=ROOT?>/assets/icons/emoji-frown.svg" style="width:100px;">
-		<h2>page not found</h2>
-	</center>
-	
-<?php require page('includes/footer')?>
